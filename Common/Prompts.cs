7010924edca3dba61c4d7ca28599c380f85aa851
Ray De Jesus <19984799+rayraydejesus@users.noreply.github.com>
2023-09-21 17:00:48 -0400

    Initial commit

---
+++ Common/Prompts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class Prompts
+{
+	public static bool oneMoreTime(string text = "\nWould you like to see another problem?")
+	{
+        Console.WriteLine(text +" (Y/N)");
+        string input = Console.ReadLine().ToLower();
+
+        if (input.Length < 1)
+            return false;
+        else if (input[0] == 'y')
+            return true;
+        else
+            return false;
+    }
+
+    public static bool anotherSolution()
+    {
+        return oneMoreTime("\n\nWould you like to try another solution?");
+    }
+
+    public static bool invalidInput()
+    {
+        Console.WriteLine("\nInvalid input.");
+        return anotherSolution();
+    }
+}
+
+
---
+++ Problems/1/Solutions/Solution_1_Hashmap.cs
@@ -0,0 +1,32 @@
+﻿public sealed class Solution_1_Hashmap: ISolution_1
+{
+	public Solution_1_Hashmap(){}
+
+	public int[] TwoSum(int[] nums, int target)
+	{
+        if (nums.Length == 2)
+            return new int[] { 0, 1 };
+
+        Dictionary<int, int> hashMap = new Dictionary<int, int>();
+        hashMap.Add(nums[0], 0);
+        int compliment;
+
+        for (int i = 1; i < nums.Length; ++i)
+        {
+            compliment = target - nums[i];
+            if (hashMap.ContainsKey(compliment))
+                return new int[] { hashMap[compliment], i };
+            else
+            {
+                if (hashMap.ContainsKey(nums[i]))
+                    hashMap[nums[i]] = i;
+                else
+                    hashMap.Add(nums[i], i);
+            }
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
+
+
