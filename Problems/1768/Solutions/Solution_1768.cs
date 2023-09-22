d896c2591c21190daa843fcb794d1ae84b286946
RYPTAR <ray.dej21@gmail.com>
2023-09-22 13:07:48 -0400

    updated solution to take advantage of StringBuilder class

--- Problems/1768/Solutions/Solution_1768.cs
+++ Problems/1768/Solutions/Solution_1768.cs
@@ -1,25 +1,33 @@
-﻿public class Solution_1768 : ISolution_1768
+﻿using System.Text;
+
+public class Solution_1768 : ISolution_1768
 {
-	public Solution_1768(){}
+    public Solution_1768() { }
 
     public string MergeAlternately(string word1, string word2)
     {
         if (word1.Length == 1)
             return word1 + word2;
 
-        string mergedWord = "";
-        int longerLength = (word1.Length > word2.Length) ? word1.Length : word2.Length;
+        StringBuilder mergedWord = new StringBuilder();
+        int shorterLength = (word1.Length < word2.Length) ? word1.Length : word2.Length;
 
-        for (int i = 0; i < longerLength; ++i)
+        for (int i = 0; i < shorterLength; ++i)
         {
-            if (i < word1.Length)
-                mergedWord = mergedWord + word1[i];
-
-            if (i < word2.Length)
-                mergedWord = mergedWord + word2[i];
+            if (i == word1.Length - 1)
+            {
+                mergedWord.Append("" + word1[i] + word2.Substring(i));
+                break;
+            }
+            else if (i == word2.Length - 1)
+            {
+                mergedWord.Append("" + word1[i] + word2[i] + word1.Substring(i + 1));
+                break;
+            }
+            else
+                mergedWord.Append("" + word1[i] + word2[i]);
         }
 
-        return mergedWord;
+        return mergedWord.ToString();
     }
 }
-
